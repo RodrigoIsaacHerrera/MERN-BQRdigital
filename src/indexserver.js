3764d331b1031af9805971ffc40767e0e2efbe11
RodrigoIsaacHerrera <rodrigoherrerabeiza01@gmail.com>
2018-11-27 02:56:28 -0300

    CDC02 comunicacion con Index.html
    ya se ha logrado establecer comunicacion entre el servidor y el navegador.

--- src/indexserver.js
+++ src/indexserver.js
@@ -2,25 +2,26 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');//este modulo se utiliza como middleware (use)
+<<<<<<< HEAD
 const path = require('path'); //permite parsear directiorios segun el sistema operativo
 
 
+=======
+>>>>>>> parent of 1596bab... Control de Cambios 02 comunicacion con Index.html
 //Settings
 app.set('port', process.env.PORT || 1989 );//pocess.env.P... permite pasar el puerto de una nunbe si la hay.
-
-
 //Middlewares
 app.use(morgan('dev'));
 app.use(express.json());//fundamental para permitir recibir y enviar json data desde server.
-
-
 //Routes
+<<<<<<< HEAD
 app.use('/api/boletos',require('./routes/tasksurl'));
 
 
+=======
+app.use('/',require('./routes/tasksurl'));
+>>>>>>> parent of 1596bab... Control de Cambios 02 comunicacion con Index.html
 //Static files
-app.use(express.static(path.join(__dirname,'public')));
-
 
 // up-server
 app.listen(app.get('port'),()=>{
