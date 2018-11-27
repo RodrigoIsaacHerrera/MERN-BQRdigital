05e4f081cb983876052b48027551e52201c7d8df
RodrigoIsaacHerrera <rodrigoherrerabeiza01@gmail.com>
2018-11-27 03:30:56 -0300

    CDC 06 reparacion de comunicacion doc HTML

--- src/indexserver.js
+++ src/indexserver.js
@@ -14,7 +14,7 @@ app.use(express.json());//fundamental para permitir recibir y enviar json data d
 app.use('/api/boletos',require('./routes/tasksurl'));
 
 //Static files
-
+app.use(express.static(path.join(__dirname,'public')));//indica donde estara el codgi standart(html,css,js)
 // up-server
 app.listen(app.get('port'),()=>{
     console.log(`Server on port ${app.get('port')} levantado!!`);
