1596babefc4e3f47bfbab36784b15a252e2af78f
RodrigoIsaacHerrera <rodrigoherrerabeiza01@gmail.com>
2018-11-26 23:42:07 -0300

    Control de Cambios 02 comunicacion con Index.html

--- src/indexserver.js
+++ src/indexserver.js
@@ -2,14 +2,25 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');//este modulo se utiliza como middleware (use)
+const path = require('path');
+
+
 //Settings
 app.set('port', process.env.PORT || 1989 );//pocess.env.P... permite pasar el puerto de una nunbe si la hay.
+
+
 //Middlewares
 app.use(morgan('dev'));
 app.use(express.json());//fundamental para permitir recibir y enviar json data desde server.
+
+
 //Routes
-app.use('/',require('./routes/tasksurl'));
+app.use('/boletos',require('./routes/tasksurl'));
+
+
 //Static files
+app.use(express.static(path.join(__dirname,'public')));
+
 
 // up-server
 app.listen(app.get('port'),()=>{
