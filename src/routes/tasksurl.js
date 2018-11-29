6b9b49f7c97758153a772614303de600f94715e1
RodrigoIsaacHerrera <rodrigoherrerabeiza01@gmail.com>
2018-11-29 02:59:29 -0300

    CDC 10
    Se integra lectura del servidor utilizando mongoose el cual asigna _id a documento json  recivido desde el cliente en el servidor.

--- src/routes/tasksurl.js
+++ src/routes/tasksurl.js
@@ -6,13 +6,18 @@ const express = require('express');
 const router = express.Router();
 const boletos =  require('../models/task')//modelo almacenado en constante para hacer consulta a la base de datos
 /*let promise = Promise((resolve, reject)=>{});promise a implementar en el futuro*/
+
+//Obtiene schema boleto
 router.get('/',async(req,res)=>{
       const  ticket  =   await boletos.find();//consulta a la db
       res.json(ticket + 'boleto en pantalla');//repuesta de la db
 });
+//Ingresa schema boleto
 router.post('/',async(req,res)=>{
-    console.log(req.body);//servidor obtiene el dato 
-    res.json('metodo post exitoso (documento creado)');//respuesta del servidor
+    const {title,description} = req.body;//cliente envia documento al servidor
+    const boleto = new boletos({title,description,});
+    console.log(boleto);
+    res.json('metodo post exitoso (documento creado)');//respuesta del servidor;
 });
 router.get('*',async(req,res)=>{
     res.json(404,'no tengo este archivo');
