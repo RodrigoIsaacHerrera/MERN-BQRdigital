aeb9c5aac368e4b45638147817d1f6ae4a64083f
RodrigoIsaacHerrera <rodrigoherrerabeiza01@gmail.com>
2018-11-30 18:24:30 -0300

    Intento de Fix
    se hara commit para intentar fixear
    comando  npm run webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,12 @@ module.exports = {
      output: {
          path: __dirname + '/src/public',
          filename: 'boundle.js'
+    },
+    module:{
+        rules:[{
+            use: 'babel-loader',
+            test :/\.js$/,
+            exclude:/node_modules/
+        }]
     }
-}
+};
