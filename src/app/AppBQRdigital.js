5e013f77b147d768be8835c6e64bc49d117cb8ff
RodrigoIsaacHerrera <rodrigoherrerabeiza01@gmail.com>
2018-12-19 08:31:14 -0300

    Footer OK

--- src/app/AppBQRdigital.js
+++ src/app/AppBQRdigital.js
@@ -352,28 +352,22 @@ class App extends Component{
                             </div>
                         </div>
                     </div>                 
-                    <footer className="page-footer">
+                    <footer className="page-footer" style={{backgroundColor:'#546e7a'}}>
                         <div className="container">
                             <div className="row">
                                 <div className="col l6 s12">
-                                    <h5 className="white-text">Footer Content</h5>
-                                    <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                                    <h5 className="white-text">BQRdigital is powered by</h5>
+                                    <p className="grey-text text-lighten-4">all love of the world.</p>
                                 </div>
                                 <div className="col l4 offset-l2 s12">
-                                    <h5 className="white-text">Links</h5>
-                                    <ul>
-                                        <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                                        <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                                        <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                                        <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
-                                    </ul>
+                                   
                                 </div>
                             </div>
                         </div>
                         <div className="footer-copyright">
                             <div className="container">
                                  © 2018 MERN-VAP-PROJECT
-                                <a className="grey-text text-lighten-4 right" href="#!">Linkedin</a>
+                                <a className="grey-text text-lighten-4 right" href="https://www.linkedin.com/in/rodrigoisaach86534b13b/">Rodrigo Herrera Beiza</a>
                             </div>
                         </div>
                     </footer>
